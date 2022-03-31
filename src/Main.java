866f100a7ba98d41800bc36f357ceb9aa0a1487a
alvglv <alvglv@gmail.com>
2022-03-31 16:50:24 +0200

    Add miles and gitignore files

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1231;
+        int rublesPerMile = 20;
+        int result;
+
+        result = ticketPrice / rublesPerMile;
+
+        System.out.println(result + " miles for ticket with the price " + ticketPrice + " rubles");
+    }
+}
